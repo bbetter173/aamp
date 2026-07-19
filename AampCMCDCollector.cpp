3277b25df9a099a52436787047a79ad96ff4d7af
Ben <ben@rilla.network>
2026-07-20 11:44:12 +1200

    Revert accidental removal of AUX_AUDIO support that was lost in refactor

--- AampCMCDCollector.cpp
+++ AampCMCDCollector.cpp
@@ -114,6 +114,18 @@ void AampCMCDCollector::Initialize(bool enableDisable , std::string &traceId)
 		pCMCDMetrics->SetMediaType("INIT_AUDIO");
 		delete mCMCDStreamData[eMEDIATYPE_INIT_AUDIO];
 		mCMCDStreamData[eMEDIATYPE_INIT_AUDIO] = pCMCDMetrics;
+		// for Aux Audio
+		pCMCDMetrics = new AudioCMCDHeaders();
+		pCMCDMetrics->SetSessionId(mTraceId);
+		pCMCDMetrics->SetMediaType("AUXAUDIO");
+		delete mCMCDStreamData[eMEDIATYPE_AUX_AUDIO];
+		mCMCDStreamData[eMEDIATYPE_AUX_AUDIO] = pCMCDMetrics;
+		// for Aux Audio Init
+		pCMCDMetrics = new AudioCMCDHeaders();
+		pCMCDMetrics->SetSessionId(mTraceId);
+		pCMCDMetrics->SetMediaType("INIT_AUDIO");
+		delete mCMCDStreamData[eMEDIATYPE_INIT_AUX_AUDIO];
+		mCMCDStreamData[eMEDIATYPE_INIT_AUX_AUDIO] = pCMCDMetrics;
 		// for Subtitle
 		pCMCDMetrics = new SubtitleCMCDHeaders();
 		pCMCDMetrics->SetSessionId(mTraceId);
